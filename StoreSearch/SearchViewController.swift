46859f030e60435281b70406f8fbcca43aff18a2
Atikur Rahman <atik.bd08@gmail.com>
2015-01-03 11:32:25 +0600

    rename UIViewController to SearchViewController

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Atikur Rahman on 1/3/15.
+//  Copyright (c) 2015 Atikur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
