62ff3aa10ea964ad50223be2ae872f2c58b23a97
Atikur Rahman <atik.bd08@gmail.com>
2015-01-05 10:39:14 +0600

    add SearchResult data model

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Atikur Rahman on 1/5/15.
+//  Copyright (c) 2015 Atikur Rahman. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
