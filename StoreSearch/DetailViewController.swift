57cff35972efcba15dcd03426850301ba0b35159
Atikur Rahman <atik.bd08@gmail.com>
2015-01-13 20:22:42 +0600

    add bounce animation to detail popup

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -92,6 +92,10 @@ extension DetailViewController: UIViewControllerTransitioningDelegate {
     func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController!, sourceViewController source: UIViewController) -> UIPresentationController? {
         return DimmingPresentationController(presentedViewController: presented, presentingViewController: presenting)
     }
+    
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return BounceAnimationController()
+    }
 }
 
 extension DetailViewController: UIGestureRecognizerDelegate {
